d2beccb0714f83a86170ad914100faa3df7bb368
Keawa Rozet <keawa.rozet@yahoo.com>
2019-04-23 14:36:50 -0700

    added buttons and they now move and show with keyboard on the AddTodoVC page.

---
+++ TodoApp/TodoApp/View Controllers/AddTodoViewController.swift
@@ -0,0 +1,60 @@
+//
+//  AddTodoViewController.swift
+//  TodoApp
+//
+//  Created by Keawa Rozet on 4/23/19.
+//  Copyright © 2019 Keawa Rozet. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+    
+    //MARK: Outlets
+
+    @IBOutlet weak var userTaskTextView: UITextView!
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(with:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    // MARK: Actions
+    
+    @objc func keyboardWillShow(with notification: Notification) {
+        let key = "UIKeyboardFrameEndUserInfoKey"
+        guard let keyboardFrame = notification.userInfo?[key] as? NSValue
+            else {
+                return
+            }
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.height + 16
+        
+        bottomConstraint.constant = keyboardHeight
+        
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @IBAction func canel(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func done(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TodoApp/TodoApp/View Controllers/TodoTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TodoTableViewController.swift
+//  TodoApp
+//
+//  Created by Keawa Rozet on 4/23/19.
+//  Copyright © 2019 Keawa Rozet. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
