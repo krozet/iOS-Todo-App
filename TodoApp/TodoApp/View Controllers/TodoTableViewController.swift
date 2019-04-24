502de9e94dfbfe8fee6de574b046345431a5a1c7
Keawa Rozet <keawa.rozet@yahoo.com>
2019-04-23 18:11:34 -0700

    preparing for sliding functionality.

--- TodoApp/TodoApp/View Controllers/TodoTableViewController.swift
+++ TodoApp/TodoApp/View Controllers/TodoTableViewController.swift
@@ -31,6 +31,16 @@ class TodoTableViewController: UITableViewController {
 
         return cell
     }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        return nil
+    }
 
     /*
     // MARK: - Navigation
